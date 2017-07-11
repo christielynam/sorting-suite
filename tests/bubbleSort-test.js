f6f043a01d72de8e8f742c16dc6d3a51bd1d3898
Christie Lynam <christielynam@gmail.com>
2017-07-10 22:49:16 -0600

    add initial testing for bubble and insertion

--- tests/bubbleSort-test.js
+++ tests/bubbleSort-test.js
@@ -5,15 +5,29 @@ describe('Bubble Sort', () => {
   it('should be a function', () => {
     expect(bubbleSort).to.be.function;
   })
+  it('should sort positive numbers', () => {
+    var numbers = [29, 12, 9, 8, 4, 21];
+    var sorted = bubbleSort(numbers);
+    expect(sorted).to.deep.equal([4, 8, 9, 12, 21, 29]);
+  });
+  it('should sort negative numbers', () => {
+    var numbers = [-29, -12, -9, -8, -4, -21];
+    var sorted = bubbleSort(numbers);
+    expect(sorted).to.deep.equal([-29, -21, -12, -9, -8, -4]);
+  });
+  it('should sort letters', () => {
+    var letters = ['d', 'b', 'a', 'c'];
+    var sorted = bubbleSort(letters);
+    expect(sorted).to.deep.equal(['a', 'b', 'c', 'd']);
+  });
+  it('should only accept arrays', () => {
+    var input = {};
+    expect(bubbleSort(input)).to.equal('This is not a valid array to be sorted!');
+    input = [4, 3, 1, 5, 2];
+    expect(bubbleSort(input)).to.deep.equal([1, 2, 3, 4, 5]);
+  });
 
 
 
-  
+
 })
-
-  // it('should sort positive numbers', () => {
-  //   var numbers = [ 5, 3, 2, 1, 4 ];
-  //   var sorted = bubbleSort(numbers);
-  //
-  //   expect(sorted).to.deep.equal([1, 2, 3, 4, 5]);
-  // });
--- tests/insertionSort-test.js
+++ tests/insertionSort-test.js
@@ -2,11 +2,37 @@ import { expect } from 'chai';
 import insertionSort from '../scripts/insertionSort';
 
 describe('Insertion Sort', () => {
+
   it('should be a function', () => {
     expect(insertionSort).to.be.function;
-  })
+  });
+
+  it('should sort positive numbers', () => {
+    var numbers = [29, 12, 9, 8, 4, 21];
+    var sorted = insertionSort(numbers);
+    expect(sorted).to.deep.equal([4, 8, 9, 12, 21, 29]);
+  });
+
+  it('should sort negative numbers', () => {
+    var numbers = [-29, -12, -9, -8, -4, -21];
+    var sorted = insertionSort(numbers);
+    expect(sorted).to.deep.equal([-29, -21, -12, -9, -8, -4]);
+  });
+
+  it('should sort letters', () => {
+    var letters = ['d', 'b', 'a', 'c'];
+    var sorted = insertionSort(letters);
+    expect(sorted).to.deep.equal(['a', 'b', 'c', 'd']);
+  });
+
+  it('should only accept arrays', () => {
+    var input = {};
+    expect(insertionSort(input)).to.equal('This is not a valid array to be sorted!');
+    input = [4, 3, 1, 5, 2];
+    expect(insertionSort(input)).to.deep.equal([1, 2, 3, 4, 5]);
+  });
 
 
 
-  
+
 })
